38d49c525a172ec2cd0e33c775f70ab3f8670219
Milena <bobkova.milena25@gmail.com>
2020-09-22 12:39:56 +0300

    Добавляет реализацию аккордеона

---
+++ build/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var accordionButtons = document.querySelectorAll('.accordion__button');
+var accordionMenues = document.querySelectorAll('.accordion__menu');
+
+var accordeonGoHandler = function (button, menu) {
+  button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    menu.classList.toggle('accordion__menu--opened')
+    button.classList.toggle('accordion__button--opened')
+  });
+}
+
+var accordionGo = function () {
+  if (accordionButtons.length > 0 && accordionMenues.length > 0) {
+    for (var i = 0; i < accordionMenues.length; i++) {
+      if (accordionButtons[i] !== null && accordionMenues[i] !== null && accordionMenues[i].childNodes.length > 0) {
+        accordeonGoHandler(accordionButtons[i], accordionMenues[i]);
+      }
+    }
+  }
+}
+
+accordionGo();
